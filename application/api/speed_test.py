f3485e86c7a6c780b896de905a511830cce58b85
ratmir <ratmir666123@gmail.com>
2025-07-08 18:02:10 +0300

    delete test file

--- application/api/speed_test.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-from domain.posts.bl import newPostBL
-speed_bp = Blueprint('speed', __name__, url_prefix='/speed')
-
-@speed_bp.route('/', methods=['GET'])
-def get_speed():
-    return jsonify(newPostBL.speed_test())
